e1e22de8d57979425d589059c7d1c6f8eff800ff
Sam <Sam@example.com>
2018-11-22 12:04:17 +0800

    polls/admin.py add class ChoiceInline

--- polls/admin.py
+++ polls/admin.py
@@ -6,14 +6,21 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
+
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     #fields = ['pub_date', 'question_text',]
 
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-
+    # ChoiceInline class 要放在QuestionAdmin上面
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
